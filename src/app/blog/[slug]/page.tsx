9cdda401f0d30beea42215183205b101f7b76818
ssanchises <ssanchises@uoc.edu>
2025-12-07 20:05:36 +0100

    update react 19 | fixed error singlepost

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,26 +1,37 @@
 // app/blog/[slug]/page.tsx
+import { use } from 'react'; // <-
 import { notFound } from "next/navigation";
 import { BLOG_POSTS } from "@/lib/data";
 import BlogPostClient from "./BlogPostClient";
 
-type PostPageProps = {
+/* type PostPageProps = {
   params: {
     slug: string;
   };
+}; */
+
+type PostPageProps = {
+  params: Promise<{ slug: string }>; // ← Es una Promise
 };
 
-export async function generateStaticParams() {
+/* export async function generateStaticParams() {
   return BLOG_POSTS.map((post) => ({
     slug: post.slug,
   }));
-}
-
+} */
 export default function SinglePostPage({ params }: PostPageProps) {
-  const post = BLOG_POSTS.find((p) => p.slug === params.slug);
+  const { slug } = use(params); // ← Usando React.use()
+  const post = BLOG_POSTS.find((p) => p.slug === slug);
 
   if (!post) {
     notFound();
   }
+/* export default function SinglePostPage({ params }: PostPageProps) {
+  const post = BLOG_POSTS.find((p) => p.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  } */
 
   // Aquí ya delegas toda la parte client con hooks
   return <BlogPostClient post={post} />;
